2b5cbfc41ec3ad6934d72d3ad27a62959deb8f6d
Lucas Carrias <lucascarrias@outlook.com>
2020-07-25 17:43:58 -0300

    Initial setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express"),
+      mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:2717', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(console.log("Connected to DB"))
+.catch(err => console.log(err));
+
+var app = express();
+const PORT = 4200;
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.get("/secret", (req, res) => {
+    res.render("secret")
+});
+
+app.listen(PORT, "localhost", () => {
+    console.log("Server connected on port: " + PORT);
+});
