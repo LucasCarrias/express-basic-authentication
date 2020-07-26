ce0fd8baead61f911082d81d04363d783b79c118
Lucas Carrias <lucascarrias@outlook.com>
2020-07-25 22:09:40 -0300

    Passport initial setups

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express"),
-      mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      bodyParser = require("body-parser"),
+      User = require("./models/user"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose");
 
 
 mongoose.connect("mongodb://localhost:2717", {
@@ -12,7 +17,15 @@ mongoose.connect("mongodb://localhost:2717", {
 var app = express();
 const PORT = 4200;
 
+app.use(require('express-session')({
+    secret: "Go go golang",
+    resave: false,
+    saveUninitialized: false
+}));
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get("/", (req, res) => {
     res.render("home");
---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose"),
+                 passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+var userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+});
+
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User", userSchema);
